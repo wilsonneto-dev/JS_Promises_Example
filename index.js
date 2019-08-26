0b1d7391300dcca355a782d6d6030322a435bcb0
Wilson Neto <contato@wilsonneto.com.br>
2019-08-26 00:46:13 -0300

    db testing

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ app = {
     teste(x) {
         return new Promise( (resolve, reject) => {
             setTimeout(() => {
-                resolve(x);
+                if(x == "r")
+                    reject(x)
+                else
+                    resolve(x);
             }, 2500);
         });
     }
@@ -12,15 +15,23 @@ console.log("iniciando...");
 
 async function f1(x){
     let num = await app.teste(x);
-    console.log(num);
+    console.log("after await....", num);
     return num;
 };
 
 try {
     let y = f1("testando");
-    console.log(y);
+    y.then(v => {
+        console.log("then...",v)
+        console.log("a var da promise é...", y);
+    });
+    console.log("a var da promise aqui é: ", y);
+
+    let z = f1("r");
+    console.log("var da promise que ser rejeitada:", z);
+
 } catch(ex){
-    console.log(ex);
+    console.log("exception...", ex);
 }
 
 console.log("finalizando...");
